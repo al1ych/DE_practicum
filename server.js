43c3612a68f5d4aca0d296fcc72ce2223e527ba7
timurnugaev <decter49pro@gmail.com>
2020-10-26 21:26:06 +0300

    test

---
+++ server.js
@@ -0,0 +1,16 @@
+const port = process.env.PORT || 3000;
+const express = require("express");
+const fs = require('fs');
+const app = express();
+const jsonParser = express.json();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json({strict: false}));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// SERVER START
+
+let server = app.listen(port, () =>
+{
+    console.log("Server started...");
+});
